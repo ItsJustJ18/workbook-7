67ded0a1b017d144ad0eec0f3a642581825dbd53
ItsJustJ18 <gaga3630@gmail.com>
2024-11-25 08:23:30 -0500

    Playing with SQL Workbench

---
+++ car-vin.sql
@@ -0,0 +1,2 @@
+vehicle_id,vin,year,make,model,vehicleType,color,odometer,price,sold
+5,34366,2021,Subaru,Skyline,Sports,"Sky Blue",6000.00,15000.00,0
---
+++ dealership.sql
@@ -0,0 +1,2 @@
+dealership_id,name,address,phone
+1,"D & B Used Cars","111 Old Benbrook Rd",-5293
